1c456cb429c6fd8cd7f37fd914af47831406da79
WolfgangPolska <wolfgangpolska@gmail.com>
2023-03-04 13:48:57 +0100

    minor refactors and documentation

--- Assets/Scripts/Classes/Static/ManagedStructsExtensions.cs
+++ Assets/Scripts/Classes/Static/ManagedStructsExtensions.cs
@@ -9,8 +9,14 @@ namespace Assets.Scripts.Classes.Static
 {
     public static class ManagedStructsExtensions
     {
+        /// <summary>
+        /// If <paramref name="number"/> is even, it returns <paramref name="number"/> incremented by 1.
+        /// </summary>
         public static int GetUnevenInteger(this int number) => number % 2 == 0 ? number + 1 : number;
 
+        /// <summary>
+        /// If <paramref name="number"/> is uneven, it returns <paramref name="number"/> incremented by 1.
+        /// </summary>
         public static int GetEvenInteger(this int number) => number % 2 == 0 ? number : number + 1;
     }
 }
--- Assets/Scripts/Managers/Rendering/TilemapShaderPropertiesManager.cs
+++ Assets/Scripts/Managers/Rendering/TilemapShaderPropertiesManager.cs
@@ -1,3 +1,4 @@
+using Assets.Scripts.Classes.Static;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -10,6 +11,6 @@ public class TilemapShaderPropertiesManager : MonoBehaviour
     {
         Tilemap tilemap = this.gameObject.GetComponent<Tilemap>();
         TilemapRenderer tilemapRenderer = this.gameObject.GetComponent<TilemapRenderer>();
-        tilemapRenderer.material.SetInteger("_MaxZ", tilemap.cellBounds.max.z % 2 != 0 ? tilemap.cellBounds.max.z + 1 : tilemap.cellBounds.max.z );
+        tilemapRenderer.material.SetInteger("_MaxZ", tilemap.cellBounds.max.z.GetUnevenInteger());
     }
 }
